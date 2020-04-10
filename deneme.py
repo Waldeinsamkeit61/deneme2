279661ae140595f7975876ae5f55f06ec70bfb7b
Waldeinsamkeit61 <fatihozoglu61@gmail.com>
2020-04-10 05:44:38 +0300

    yeni eklendi

--- deneme.py
+++ deneme.py
@@ -1,4 +1,6 @@
-Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def cikarma(x,y):
+def cikarma(x,y):
 	return x-y
+def merhaba():
+    print("Merhaba Dünya")
+def toplama(a,b):
+    return x+y
